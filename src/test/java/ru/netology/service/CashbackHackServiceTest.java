624107ae2347f494f759873aebf87ddae75d5327
JMatweewa <93136237+JMatweewa@users.noreply.github.com>
2022-04-08 23:13:49 +0700

    added JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Test;
 
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService cashbackHackService = new CashbackHackService();
@@ -10,42 +10,42 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldCalculateRemainAverageValue() {
         int amount = 550;
-        Assert.assertEquals(cashbackHackService.remain(amount), 450);
+        assertEquals(450, cashbackHackService.remain(amount));
     }
 
     @Test
     public void shouldCalculateRemainValueNull() {
         int amount = 0;
-        Assert.assertEquals(cashbackHackService.remain(amount), 1000);
+        assertEquals(1000, cashbackHackService.remain(amount));
     }
 
     @Test
     public void shouldCalculateRemainValue1() {
         int amount = 1;
-        Assert.assertEquals(cashbackHackService.remain(amount), 999);
+        assertEquals(999, cashbackHackService.remain(amount));
     }
 
     @Test
     public void shouldCalculateRemainNegativeValue() {
         int amount = -1; // производит расчет при отрицательном балансе, желательно проверить требования
-        Assert.assertEquals(cashbackHackService.remain(amount), 1001);
+        assertEquals(1001, cashbackHackService.remain(amount));
     }
 
     @Test
     public void shouldCalculateRemainValue999() {
         int amount = 999;
-        Assert.assertEquals(cashbackHackService.remain(amount), 1);
+        assertEquals(1, cashbackHackService.remain(amount));
     }
 
     @Test
     public void shouldCalculateRemainValueFinalBoundary1000() {
         int amount = 1000; // при покупке ровно на 1000р предлагает купить ещё на 1000р.
-        Assert.assertEquals(cashbackHackService.remain(amount), 0);
+        assertEquals(0, cashbackHackService.remain(amount));
     }
 
     @Test
     public void shouldCalculateRemainValue1001() {
         int amount = 1001;
-        Assert.assertEquals(cashbackHackService.remain(amount), 999);
+        assertEquals(999, cashbackHackService.remain(amount));
     }
 }
